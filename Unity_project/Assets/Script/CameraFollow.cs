d2d91a1375f359e1d68ca54c1761294e83903168
Phil Gosch <phil@saphirestudio.at>
2017-12-27 17:42:10 +0100

    Fix camera jittering

--- Unity_project/Assets/Script/CameraFollow.cs
+++ Unity_project/Assets/Script/CameraFollow.cs
@@ -4,21 +4,16 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
     public Transform _target;
-
     public float _smoothingFactor = 1.0f;
-
     public Player_Input _pInput;
-
     public float _aimDeviation = 3.0f;
+    public float _aimThreshold = 0.5f;
 
     private Vector3 offset;
 
-    private Transform camPos;
-
     void Start()
     {
-        camPos = gameObject.transform; //cache
-
+        //TODO: set this explicitely once start menu is refactored/redone
         offset = gameObject.transform.position - _target.position;
     }
 
@@ -26,13 +21,21 @@ public class CameraFollow : MonoBehaviour {
     {
         Vector3 aim_dir = _pInput.GetMousePos() - _target.position;
 
-        if(aim_dir.magnitude > _aimDeviation)
+        //follow player pos if cursor near player character to fix jittering
+        if (aim_dir.magnitude < _aimThreshold)
         {
-            aim_dir = aim_dir.normalized * _aimDeviation;
+            transform.position = _target.position;
         }
+        else
+        {
+            //limit boundaries of camera movement relative to player-cursor distance
+            if (aim_dir.magnitude > _aimDeviation)
+            {
+                aim_dir = aim_dir.normalized * _aimDeviation;
+            }
 
-        Vector3 delta = offset + _target.position + aim_dir;
-        camPos.position = delta;
-        //camPos.position = Vector3.Lerp(camPos.position, delta, _smoothingFactor * Time.deltaTime);
+            Vector3 delta = offset + _target.position + aim_dir;
+            transform.position = delta;
+        }
     }
 }
